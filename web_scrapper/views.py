9a5487ca093a851ece437589edf40c4283581587
robertw <rwrzosu@gmail.com>
2017-04-01 14:09:14 +0200

    added Bootstrap 4 and a few touch ups

--- web_scrapper/views.py
+++ web_scrapper/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import SearchProfile
 
+
 class SearchProfileListView(ListView):
     template_name = 'web_scrapper/list.html'
     context_object_name = 'search_profile_list'
     model = SearchProfile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['section'] = 'web_scrapper'
+        return context
+
 
 # Create your views here.
 def configure(request):
